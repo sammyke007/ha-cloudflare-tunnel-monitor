acf9cbbc8adbaff073f3d815a04d09f9230112f8
Sammy Verdonck <verdonck.sammy@gmail.com>
2025-12-07 21:51:48 +0100

    Hassfest validation

--- custom_components/cloudflare_tunnel_monitor/__init__.py
+++ custom_components/cloudflare_tunnel_monitor/__init__.py
@@ -1,11 +1,17 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
+import homeassistant.helpers.config_validation as cv
 
 from .const import DOMAIN
+
+# Required because async_setup() exists, and this integration is config-entry only.
+CONFIG_SCHEMA = cv.config_entry_only_config_schema(DOMAIN)
+
 
 async def async_setup(hass: HomeAssistant, config: dict):
     """Set up Cloudflare Tunnel Monitor component."""
     return True
+
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
     """Set up integration from a config entry."""
@@ -19,6 +25,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
 
     await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
     return True
+
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry):
     unload_ok = await hass.config_entries.async_unload_platforms(entry, ["sensor"])
